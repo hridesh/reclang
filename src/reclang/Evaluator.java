cbf61571f61355239eaf319987fd6f170005968b
Hridesh Rajan <hridesh@gmail.com>
2017-12-30 11:37:26 -0600

    semantics update

--- src/reclang/Evaluator.java
+++ src/reclang/Evaluator.java
@@ -5,6 +5,7 @@ import static reclang.Value.*;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.File;
+import java.io.IOException;
 
 import reclang.Env.*;
 
@@ -30,22 +31,22 @@ public class Evaluator implements Visitor<Value> {
 	}
 	
 	@Override
-	public Value visit(Unit e, Env env) {
+	public Value visit(UnitExp e, Env env) {
 		return new UnitVal();
 	}
 
 	@Override
-	public Value visit(Const e, Env env) {
+	public Value visit(NumExp e, Env env) {
 		return new NumVal(e.v());
 	}
 
 	@Override
-	public Value visit(StrConst e, Env env) {
+	public Value visit(StrExp e, Env env) {
 		return new StringVal(e.v());
 	}
 
 	@Override
-	public Value visit(BoolConst e, Env env) {
+	public Value visit(BoolExp e, Env env) {
 		return new BoolVal(e.v());
 	}
 
@@ -218,12 +219,6 @@ public class Evaluator implements Visitor<Value> {
 	@Override
 	public Value visit(CdrExp e, Env env) { 
 		Value.PairVal pair = (Value.PairVal) e.arg().accept(this, env);
-		Value result = pair.snd();
-		//Special case for list: cdr of a list is a list also.
-		if(pair instanceof Value.ExtendList) {
-			if(result instanceof Value.EmptyList) return result;
-			return new ExtendList(((Value.PairVal)result).fst(),((Value.PairVal)result).snd());
-		}
 		return pair.snd();
 	}
 	
@@ -231,14 +226,6 @@ public class Evaluator implements Visitor<Value> {
 	public Value visit(ConsExp e, Env env) { 
 		Value first = (Value) e.fst().accept(this, env);
 		Value second = (Value) e.snd().accept(this, env);
-		//Special case for list: cdr of a list is a list also.
-		if(second instanceof Value.EmptyList) 
-			return new Value.ExtendList(first, second);
-		else if(second instanceof Value.ExtendList) {
-			ExtendList rest = (Value.ExtendList) second;
-			PairVal newSecond = new PairVal(rest.fst(), rest.snd());
-			return new ExtendList(first, newSecond);
-		}
 		return new Value.PairVal(first, second);
 	}
 
@@ -247,27 +234,23 @@ public class Evaluator implements Visitor<Value> {
 		List<Exp> elemExps = e.elems();
 		int length = elemExps.size();
 		if(length == 0)
-			return new Value.EmptyList();
+			return new Value.Null();
 		
-		List<Value> elems = new ArrayList<Value>(length);
-		for(Exp exp : elemExps) 
-			elems.add((Value) exp.accept(this, env));
+		//Order of evaluation: left to right e.g. (list (+ 3 4) (+ 5 4)) 
+		Value[] elems = new Value[length];
+		for(int i=0; i<length; i++)
+			elems[i] = (Value) elemExps.get(i).accept(this, env);
 		
-		Value.PairVal list = null;
-		for(int i=length-1; i>0; i--) {
-			if(list == null)
-				list = new PairVal(elems.get(i), new Value.EmptyList());
-			else list = new PairVal(elems.get(i),list);
-		}
-		if(list == null) list = new ExtendList(elems.get(0), new Value.EmptyList());
-		else list = new ExtendList(elems.get(0),list);
-		return list;
+		Value result = new Value.Null();
+		for(int i=length-1; i>=0; i--) 
+			result = new PairVal(elems[i], result);
+		return result;
 	}
 	
 	@Override
 	public Value visit(NullExp e, Env env) {
 		Value val = (Value) e.arg().accept(this, env);
-		return new BoolVal(val instanceof Value.EmptyList);
+		return new BoolVal(val instanceof Value.Null);
 	}
 
 	public Value visit(EvalExp e, Env env) {
@@ -278,8 +261,12 @@ public class Evaluator implements Visitor<Value> {
 
 	public Value visit(ReadExp e, Env env) {
 		StringVal fileName = (StringVal) e.file().accept(this, env);
-		String text = Reader.readFile("" + System.getProperty("user.dir") + File.separator + fileName.v());
-		return new StringVal(text);
+		try {
+			String text = Reader.readFile("" + System.getProperty("user.dir") + File.separator + fileName.v());
+			return new StringVal(text);
+		} catch (IOException ex) {
+			return new DynamicError(ex.getMessage());
+		}
 	}
 	
 	@Override
